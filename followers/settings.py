edd3e3a8dccf8acb1a9932a301bcba7d038afce2
julianolm <65794514+julianolm@users.noreply.github.com>
2021-09-06 22:54:44 -0300

    update template to stop using static

--- followers/settings.py
+++ followers/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/dnfb/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
